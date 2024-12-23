cd94b556bfdd302e503b2458cbe01fd22f09cfa3
Michał Łuczak <mluczak@alan-systems.com>
2024-12-23 16:07:13 +0100

    Refactor: Imports cleansing

--- composeApp/src/androidMain/kotlin/com/miluconnect/profeliomp/di/initKoin.android.kt
+++ composeApp/src/androidMain/kotlin/com/miluconnect/profeliomp/di/initKoin.android.kt
@@ -1,10 +1,5 @@
 package com.miluconnect.profeliomp.di
 
-import android.content.Context
-import android.util.Log
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
-
 fun initKoinAndroid() {
     initCommonKoin(androidModule)
     println("KOIN initKoinAndroid()")
--- composeApp/src/iosMain/kotlin/com/miluconnect/profeliomp/di/initKoin.ios.kt
+++ composeApp/src/iosMain/kotlin/com/miluconnect/profeliomp/di/initKoin.ios.kt
@@ -1,7 +1,5 @@
 package com.miluconnect.profeliomp.di
 
-import org.koin.core.context.startKoin
-
 fun initKoinIOS() {
     initCommonKoin(iosModule)
     println("KOIN initKoinIOS")
