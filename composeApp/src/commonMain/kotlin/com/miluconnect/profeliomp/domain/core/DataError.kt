f0f58231dd28a8194e2a2d3859662391212fcd81
jackyonderbrack <mike.projektowanie@gmail.com>
2024-12-27 16:08:48 +0100

    Feature: Interface sealed DataError added

---
+++ composeApp/src/commonMain/kotlin/com/miluconnect/profeliomp/domain/core/DataError.kt
@@ -0,0 +1,23 @@
+package com.miluconnect.profeliomp.domain.core
+
+sealed interface DataError: Error {
+    enum class Remote: DataError {
+        REQUEST_TIMEOUT,
+        TOO_MANY_REQUESTS,
+        NOT_FOUND,
+        NO_INTERNET,
+        UNAUTHORIZED,
+        SERVER_ERROR,
+        NOT_IMPLEMENTED,
+        BAD_GATEWAY,
+        SERVICE_UNAVAILABLE,
+        GATEWAY_TIMEOUT,
+        SERIALIZATION,
+        UNKNOWN
+    }
+
+    enum class Local: DataError {
+        DISK_FULL,
+        UNKNOWN
+    }
+}
