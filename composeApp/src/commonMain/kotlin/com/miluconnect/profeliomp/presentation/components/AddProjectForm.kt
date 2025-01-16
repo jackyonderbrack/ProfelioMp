cdeb20433df8332ee20e0fe39bd16a968b9a0de2
jackyonderbrack <mike.projektowanie@gmail.com>
2025-01-16 21:18:34 +0100

    Style: Button create and dissmis on project form styling

--- composeApp/src/commonMain/kotlin/com/miluconnect/profeliomp/presentation/components/AddProjectForm.kt
+++ composeApp/src/commonMain/kotlin/com/miluconnect/profeliomp/presentation/components/AddProjectForm.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
@@ -32,6 +33,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierInfo
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -247,11 +249,13 @@ fun AddProjectForm(
                 horizontalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 Button(
+                    modifier = Modifier.weight(1f).height(48.dp),
                     onClick = {},
                     label = stringResource(Res.string.new_project_dismiss_button_label),
                     buttonType = ButtonType.TEXT
                 )
                 Button(
+                    modifier = Modifier.weight(1f).height(48.dp),
                     onClick = {},
                     label = stringResource(Res.string.new_project_create_button_label)
                 )
