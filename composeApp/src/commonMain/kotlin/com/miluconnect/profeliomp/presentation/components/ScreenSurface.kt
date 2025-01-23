bf4f2e16f4506d18ff8c9da803b78fb51f281e8f
Michał Łuczak <mluczak@alan-systems.com>
2025-01-23 13:20:02 +0100

    Feature: Preview Delete

--- composeApp/src/commonMain/kotlin/com/miluconnect/profeliomp/presentation/components/ScreenSurface.kt
+++ composeApp/src/commonMain/kotlin/com/miluconnect/profeliomp/presentation/components/ScreenSurface.kt
@@ -1,10 +1,8 @@
 package com.miluconnect.profeliomp.presentation.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
