374acd280d4f20b557b3595aabb2bfaf7fed97ab
jackyonderbrack <mike.projektowanie@gmail.com>
2025-01-31 22:44:22 +0100

    Feature: Media nullable to empty list

--- composeApp/src/commonMain/kotlin/com/miluconnect/profeliomp/data/mappers/ProjectMapper.kt
+++ composeApp/src/commonMain/kotlin/com/miluconnect/profeliomp/data/mappers/ProjectMapper.kt
@@ -13,6 +13,6 @@ fun ProjectDto.toProjectModel(): Project {
         city = city,
         status = status,
         labels = labels,
-        media = media?.map { it.toMediaModel() }
+        media = media?.map { it.toMediaModel() } ?: emptyList()
     )
 }
