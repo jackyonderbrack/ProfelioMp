3b29d81c2af7f9e64da6e75490f0dff0ae475751
jackyonderbrack <mike.projektowanie@gmail.com>
2025-02-06 22:33:46 +0100

    Feature: No projects or discussion presentation

--- composeApp/src/commonMain/kotlin/com/miluconnect/profeliomp/presentation/screens/work/components/IssuesList.kt
+++ composeApp/src/commonMain/kotlin/com/miluconnect/profeliomp/presentation/screens/work/components/IssuesList.kt
@@ -1,10 +1,14 @@
 package com.miluconnect.profeliomp.presentation.screens.work.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -27,16 +31,26 @@ fun IssuesList(
         }
     }
 
-    LazyColumn(
-        modifier = modifier.fillMaxWidth(),
-        state = scrollState,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        items(
-            items = sortedProjectList,
-            key = { it.id!! }
-        ) { item ->
-            IssuesListItem(issueItem = item)
+    if (issuesList.isNotEmpty()) {
+        LazyColumn(
+            modifier = modifier.fillMaxWidth(),
+            state = scrollState,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(
+                items = sortedProjectList,
+                key = { it.id!! }
+            ) { item ->
+                IssuesListItem(issueItem = item)
+            }
+        }
+    } else {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "You didn't create any issue yet")
         }
     }
 }
--- composeApp/src/commonMain/kotlin/com/miluconnect/profeliomp/presentation/screens/work/components/ProjectsList.kt
+++ composeApp/src/commonMain/kotlin/com/miluconnect/profeliomp/presentation/screens/work/components/ProjectsList.kt
@@ -1,10 +1,15 @@
 package com.miluconnect.profeliomp.presentation.screens.work.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -28,19 +33,29 @@ fun ProjectsList(
         }
     }
 
-    LazyColumn(
-        modifier = modifier.fillMaxWidth(),
-        state = scrollState,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        items(
-            items = sortedProjectList,
-            key = { it.id!! }
-        ) { item ->
-            ProjectListItem(
-                projectItem = item,
-                onItemClick = onListClick // pass callback
-            )
+    if (projectList.isNotEmpty()) {
+        LazyColumn(
+            modifier = modifier.fillMaxWidth(),
+            state = scrollState,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(
+                items = sortedProjectList,
+                key = { it.id!! }
+            ) { item ->
+                ProjectListItem(
+                    projectItem = item,
+                    onItemClick = onListClick // pass callback
+                )
+            }
+        }
+    } else {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "No projects found. Try creating one.")
         }
     }
 }
